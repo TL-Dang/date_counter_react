4484564b308a635bfddc3bca2480104958b7d801
Tommy Dang <ledangt310@live.com>
2023-06-22 14:18:15 -0700

    add dial, input field

--- date_counter/src/App.js
+++ date_counter/src/App.js
@@ -13,6 +13,11 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+  function resetHandler() {
+    setCount(0);
+    setStep(1);
+  }
+
   const date = new Date('June 29 2023');
   date.setDate(date.getDate() + count);
 
@@ -23,11 +28,29 @@ function Counter() {
         <span>Step: {step}</span>
         <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
-      
+
+      <div>
+        <input
+          type='range'
+          min='0'
+          max='10'
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+      </div>
+
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count: {count}</span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <div>
+        <input
+          type='text'
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
       </div>
 
       <span>
@@ -35,10 +58,16 @@ function Counter() {
           ? 'Today is '
           : count > 0
           ? `${count} days from today is`
-          : `${count} days ago was `}
+          : `${Math.abs(count)} days ago was `}
       </span>
       <span>
         <h3>{date.toDateString()}</h3>
+
+        {count !== 0 || step !== 1 ? (
+          <div>
+            <button onClick={resetHandler}>Reset</button>
+          </div>
+        ) : null}
       </span>
     </div>
   );
